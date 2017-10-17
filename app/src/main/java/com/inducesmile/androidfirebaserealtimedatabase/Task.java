edc59ff398f1591af46502dc6713b15b0089d37d
Jakub Wójcik <wujek444@gmail.com>
2017-10-18 00:28:16 +0200

    Initial commit

---
+++ app/src/main/java/com/inducesmile/androidfirebaserealtimedatabase/Task.java
@@ -0,0 +1,17 @@
+package com.inducesmile.androidfirebaserealtimedatabase;
+
+/**
+ * Created by jrwoj on 17.10.2017.
+ */
+
+public class Task {
+    private String task;
+
+    public Task() {}
+    public Task(String task) {
+        this.task = task;
+    }
+    public String getTask() {
+        return task;
+    }
+}
